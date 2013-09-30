3d2a0fbf67b3f3ab18d106230995351a8c8016e9
md_5 <md_5@live.com.au>
2013-09-30 21:06:39 +1000

    Update for shorter urls and stuff

--- screenshot.sh
+++ screenshot.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
-BUCKET="au.files.md-5.net"
+BUCKET="au.files.md-5.net/s"
+URL_BASE="http://files.md-5.net/s"
 FILE_DIR="$HOME/Pictures/Screenshots"
 ###############################################################################
 ###############################################################################
 ###############################################################################
-FILE_NAME="$(cat /dev/urandom | tr -c -d '[:alnum:]' | head -c 5).png"
-FILE_PATH="$FILE_DIR/$FILE_NAME"
+
+while true
+do
+	FILE_NAME="$(cat /dev/urandom | tr -c -d '[:alnum:]' | head -c 4).png"
+	FILE_PATH="$FILE_DIR/$FILE_NAME"
+	if [ ! -f "$FILE_PATH" ]; then break; fi
+done
 
 mkdir -p "$FILE_DIR"
 gnome-screenshot -a -f "$FILE_PATH"
 
 if [ -f "$FILE_PATH" ]
 then
-	s3cmd sync "$FILE_DIR/" "s3://$BUCKET"
-	UPLOAD_URL="http://$BUCKET/$FILE_NAME"
+	s3cmd sync "$FILE_DIR/" "s3://$BUCKET/"
+	UPLOAD_URL="$URL_BASE/$FILE_NAME"
 	echo -n "$UPLOAD_URL" | xsel -b -i
 	notify-send Screenshot "$UPLOAD_URL copied to clipboard" -i "$FILE_PATH" -t 1000
 fi
